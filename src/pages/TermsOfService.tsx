d190dc153743d6d1f0d05e68b8c06ecb087abe11
leogrundstrom <leo@kloudboard.com>
2026-09-11 21:43:42 +0400

    Correct legal operator and SMS policy disclosures

--- src/pages/TermsOfService.tsx
+++ src/pages/TermsOfService.tsx
@@ -42,7 +42,7 @@ const TermsOfService = () => {
           <CardContent className="prose prose-sm max-w-none dark:prose-invert">
             <h2>OVERVIEW</h2>
             <p>
-              This website is operated by MonetizedProfiles. Throughout the site, the terms "we", "us" and "our" refer to MonetizedProfiles. MonetizedProfiles offers this website, including all information, tools and Services available from this site to you, the user, conditioned upon your acceptance of all terms, conditions, policies and notices stated here.
+              This website is operated by Ascentir Management Consultancies FZCO (registration number 49188), trading as MonetizedProfiles. Throughout the site, the terms "we", "us" and "our" refer to this operator. MonetizedProfiles offers this website, including all information, tools and Services available from this site to you, the user, conditioned upon your acceptance of all terms, conditions, policies and notices stated here.
             </p>
             <p>
               By visiting our site and/or purchasing something from us, you engage in our "Service" and agree to be bound by the following terms and conditions ("Terms of Service", "Terms"), including those additional terms and conditions and policies referenced herein and/or available by hyperlink. These Terms of Service apply to all users of the site, including without limitation users who are browsers, vendors, customers, merchants, and/or contributors of content.
